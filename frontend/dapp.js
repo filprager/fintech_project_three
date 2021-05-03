315a23e7ccf91c484f3465d2f929daaf11234b95
marksenyang <marksenyang@gmail.com>
2021-05-03 13:18:03 +1000

    Updated ABI

--- frontend/dapp.js
+++ frontend/dapp.js
@@ -1,5 +1,5 @@
 // @TODO: Update this address to match your deployed TaskMarket contract!
-const contractAddress = "0xe51Bfb55FB318e22A9C4416F98421BA57f0c364a";
+const contractAddress = "0x7C811d8c4804ab2B4EF92ED7A5EF8C7D30d267dD";
 
 
 const dApp = {
